eb0008d2b59f0139063fcbca6e3c5bc504e2d03f
Nicolas Roger <nramonteirods05@gmail.com>
2026-05-12 20:28:26 -0300

    Add files via upload

---
+++ projeto_calculadora_com_testes/src/main/java/com/example/projeto_calculadora_com_testes/ProjetoCalculadoraComTestesApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_calculadora_com_testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCalculadoraComTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCalculadoraComTestesApplication.class, args);
+	}
+
+}
---
+++ projeto_calculadora_com_testes/src/test/java/com/example/projeto_calculadora_com_testes/ProjetoCalculadoraComTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_calculadora_com_testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCalculadoraComTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
